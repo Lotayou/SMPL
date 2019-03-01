0c9c57999e534b107a062b667c6151b3037b9092
Eric Yang <lotayou@pku.edu.cn>
2019-03-01 16:05:57 +0800

    Update training scipt for 24_joint representation

--- utils.py
+++ utils.py
@@ -17,7 +17,7 @@ def create_dataset(num_samples, dataset_name, batch_size=32, theta_var=1.0, gpu_
     beta_size = 10
 
     np.random.seed(9608)
-    model = SMPLModel(device=device)
+    model = SMPLModel(device=device, model_path = 'model_24_joints.pkl')
     
     d_poses = torch.from_numpy((np.random.rand(num_samples, pose_size) - 0.5) * theta_var)\
               .type(torch.float64).to(device)
@@ -51,4 +51,4 @@ def create_dataset(num_samples, dataset_name, batch_size=32, theta_var=1.0, gpu_
 if __name__ == '__main__':
     for tv in [0.2, 0.4, 0.6, 0.8, 1.0]:
         create_dataset(num_samples=262144, batch_size=64, theta_var=tv, 
-            dataset_name='./train_dataset_{}.pickle'.format(tv))
+            dataset_name='./train_dataset_24_joints_{}.pickle'.format(tv))
