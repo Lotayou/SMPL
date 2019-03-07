6147dd822d0addeb8fd38e818da434fec73ec19f
Eric Yang <lotayou@pku.edu.cn>
2019-03-07 16:14:44 +0800

    Reformat SMPLModel: Isolate theta2G module from forward function for better extendibility.

--- smpl_torch_batch.py
+++ smpl_torch_batch.py
@@ -124,52 +124,11 @@ class SMPLModel(Module):
 
   def visualize_model_parameters(self):
     self.write_obj(self.v_template, 'v_template.obj')
-        
-  def forward(self, betas, pose, trans):
     
-    """
-          Construct a compute graph that takes in parameters and outputs a tensor as
-          model vertices. Face indices are also returned as a numpy ndarray.
-          
-          20190128: Add batch support.
-
-          Parameters:
-          ---------
-          pose: Also known as 'theta', an [N, 24, 3] tensor indicating child joint rotation
-          relative to parent joint. For root joint it's global orientation.
-          Represented in a axis-angle format.
-
-          betas: Parameter for model shape. A tensor of shape [N, 10] as coefficients of
-          PCA components. Only 10 components were released by SMPL author.
-
-          trans: Global translation tensor of shape [N, 3].
-
-          Return:
-          ------
-          A 3-D tensor of [N * 6890 * 3] for vertices,
-          and the corresponding [N * 19 * 3] joint positions.
-
-    """
-    batch_num = betas.shape[0]
-    
-    v_shaped = torch.tensordot(betas, self.shapedirs, dims=([1], [2])) + self.v_template
-    J = torch.matmul(self.J_regressor, v_shaped)
-    R_cube_big = self.rodrigues(pose.view(-1, 1, 3)).reshape(batch_num, -1, 3, 3)
-
-    
-    # (1) Pose shape blending (SMPL formula(9))
-    if self.simplify:
-      v_posed = v_shaped
-    else:
-      R_cube = R_cube_big[:, 1:, :, :]
-      I_cube = (torch.eye(3, dtype=torch.float64).unsqueeze(dim=0) + \
-        torch.zeros((batch_num, R_cube.shape[1], 3, 3), dtype=torch.float64)).to(self.device)
-      lrotmin = (R_cube - I_cube).reshape(batch_num, -1, 1).squeeze(dim=2)
-      v_posed = v_shaped + torch.tensordot(lrotmin, self.posedirs, dims=([1], [2]))
-
-      
-    # (2) Skinning (W)
-    results = []
+  def theta2G(self, thetas, J):
+    batch_num = thetas.shape[0]
+    R_cube_big = self.rodrigues(thetas.view(-1, 1, 3)).reshape(batch_num, -1, 3, 3)
+    results = []    # results correspond to G terms in original paper.
     results.append(
       self.with_zeros(torch.cat((R_cube_big[:, 0], torch.reshape(J[:, 0, :], (-1, 3, 1))), dim=2))
     )
@@ -197,8 +156,51 @@ class SMPLModel(Module):
           )
         )
       )
-    # Restart from here
-    T = torch.tensordot(results, self.weights, dims=([1], [1])).permute(0, 3, 1, 2)
+    return results, R_cube_big
+  
+  def forward(self, betas, thetas, trans):
+    
+    """
+          Construct a compute graph that takes in parameters and outputs a tensor as
+          model vertices. Face indices are also returned as a numpy ndarray.
+          
+          20190128: Add batch support.
+
+          Parameters:
+          ---------
+          thetas: an [N, 24 * 3] tensor indicating child joint rotation
+          relative to parent joint. For root joint it's global orientation.
+          Represented in a axis-angle format.
+
+          betas: Parameter for model shape. A tensor of shape [N, 10] as coefficients of
+          PCA components. Only 10 components were released by SMPL author.
+
+          trans: Global translation tensor of shape [N, 3].
+
+          Return:
+          ------
+          A 3-D tensor of [N * 6890 * 3] for vertices,
+          and the corresponding [N * 24 * 3] joint positions.
+
+    """
+    batch_num = betas.shape[0]
+    
+    v_shaped = torch.tensordot(betas, self.shapedirs, dims=([1], [2])) + self.v_template
+    J = torch.matmul(self.J_regressor, v_shaped)
+    G, R_cube_big = self.theta2G(thetas, J)  # pre-calculate G terms for skinning 
+    
+    # (1) Pose shape blending (SMPL formula(9))
+    if self.simplify:
+      v_posed = v_shaped
+    else:
+      R_cube = R_cube_big[:, 1:, :, :]
+      I_cube = (torch.eye(3, dtype=torch.float64).unsqueeze(dim=0) + \
+        torch.zeros((batch_num, R_cube.shape[1], 3, 3), dtype=torch.float64)).to(self.device)
+      lrotmin = (R_cube - I_cube).reshape(batch_num, -1, 1).squeeze(dim=2)
+      v_posed = v_shaped + torch.tensordot(lrotmin, self.posedirs, dims=([1], [2]))
+      
+    # (2) Skinning (W)
+    T = torch.tensordot(G, self.weights, dims=([1], [1])).permute(0, 3, 1, 2)
     rest_shape_h = torch.cat(
       (v_posed, torch.ones((batch_num, v_posed.shape[1], 1), dtype=torch.float64).to(self.device)), dim=2
     )
